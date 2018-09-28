ff254a75c77f050fdf2e6aa2420100f09057d8e4
Rob Lahoda <rob@roblahoda.com>
2018-09-28 14:23:52 -0400

    added flexbox fallback to header screen area

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,21 +1,4 @@
 
-// function boxesHeaderLoop() {
-//   let boxes = document.getElementsByClassName("nameImage__container--box");
-//   console.log(boxes);
-//   let boxesTL = new TimelineMax({repeat:2, repeatDelay:1});
-//   for (var i = 0; i < boxes.length; i++) {
-//     boxesTL.to(boxes[i], 1.5, {opacity:0});
-//     console.log(i);
-//   }
-// }
-//
-// TweenMax.staggerFrom(".nameImage__container--box", 2, {opacity:1, yoyo:true, repeat:100, repeatDelay:1}, Math.random() * 2 );
-
-// headerBoxes.addEventListener("click", boxesHeaderFade, false);
-
-// Array.from(headerBoxes).forEach(function(element) {
-//       element.addEventListener('click', boxesHeaderFade);
-//     });
 
 function getRandom(min,max) {
   let randomNumber = Math.floor(Math.random() * (max-min)) + 1;
@@ -23,20 +6,20 @@ function getRandom(min,max) {
 }
 
 function boxesHeaderRandom() {
-  console.log("yep");
-  let randomNumber = getRandom(5,10);
   let tl = new TimelineMax();
-  console.log(randomNumber);
-  for (var i = 0; i < randomNumber; i++) {
-    let randomId = getRandom(1,30);
+  for (var i = 0; i < 10; i++) {
+    let randomId = getRandom(1,28);
     let elId = "#nameImage__container--box--" + randomId;
     tl.to(elId, 2, {opacity:0});
     tl.to(elId, 2, {opacity:1});
-    console.log(randomId);
   }
+  tl.eventCallback("onComplete", boxesHeaderRandom);
 }
 
-window.onload = boxesHeaderRandom;
+window.onload = function(){
+  boxesHeaderRandom();
+  pageTaglineLoop();
+}
 
 function boxesHeaderFade(id) {
   let tl = new TimelineMax();
@@ -47,3 +30,17 @@ function boxesHeaderFade(id) {
   tl.to(elId, 1.5, {opacity:1});
   // TweenMax.to(elId, 1.5, {opacity:0,repeat:1,repeatDelay:1, yoyo:true});
 }
+
+// window.onload = pageTaglineLoop;
+
+function pageTaglineLoop() {
+  let tagLines = ["I Tell Stories", "I Theme In Drupal", "I Take Pictures", "I Make Videos", "I Like Pizza", "I Build Websites"];
+  let tl = new TimelineMax({repeat: -1});
+  tl.delay(2);
+  for (var i = 0; i < tagLines.length; i++) {
+    let tagline = tagLines[i];
+    // console.log(tagline);
+    tl.to(".pageTagline", 1, {text:tagline, ease:Linear.easeNone, delay:2});
+
+  }
+  }
