8ebacc87619cd7e8e371b35a73bd557f194ce18d
Rob Lahoda <rob@roblahoda.com>
2018-10-03 14:51:27 -0400

    added vendor directory, finished tweaking content for initial launch, checked on multiple devices for compatibility

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -25,21 +25,18 @@ window.onload = function(){
 
 function boxesHeaderFade(id) {
   let tl = new TimelineMax();
-  // console.log("yep");
   let elId = "#"+id;
-  // console.log(elId);
   tl.to(elId, 1.5, {opacity:0});
   tl.to(elId, 1.5, {opacity:1});
 }
 
 
 function pageTaglineLoop() {
-  let tagLines = ["I Tell Stories", "I Theme In Drupal", "I Take Pictures", "I Make Videos", "I Like Pizza", "I Build Websites"];
+  let tagLines = ["I Tell Stories", "I Theme In Drupal", "I Take Pictures", "I'm A Visual Storyteller", "I Make Videos", "I Like Pizza", "I Build Websites"];
   let tl = new TimelineMax({repeat: -1});
   tl.delay(2);
   for (var i = 0; i < tagLines.length; i++) {
     let tagline = tagLines[i];
-    // console.log(tagline);
     tl.to("#indexTagline", 1, {text:tagline, ease:Linear.easeNone, delay:2});
 
   }
@@ -57,13 +54,11 @@ function navListAnimate() {
     let tl = new TimelineMax();
 
   if (direction.classList.contains("navList_in")) {
-    // console.log("yep");
       tl.staggerTo(".navList__navItem",0.5,{opacity:1, width: "auto", margin: "10px 25px"}, 0.1);
       tl.to("#navList__menuLink--arrow",0.4,{text:"⇨"});
     direction.classList.remove('navList_in');
     direction.classList.add('navList_out');
   } else if (direction.classList.contains("navList_out")) {
-    // console.log("out");
       tl.staggerTo(".navList__navItem",0.5,{opacity:0, width: 0, margin: "10px 0" }, 0.1);
       tl.to("#navList__menuLink--arrow",0.4,{text:"⇦"});
     direction.classList.remove('navList_out');
